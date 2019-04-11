1cd4cf2419cc4b87f898e8ea579167a9ec8add4a
LiangYT <381518188@qq.com>
2019-04-11 18:14:30 +0800

    添加截图

--- app/src/main/java/com/sinothk/view/image/rounded/demo/RoundedDemoMainActivity.java
+++ app/src/main/java/com/sinothk/view/image/rounded/demo/RoundedDemoMainActivity.java
@@ -3,11 +3,16 @@ package com.sinothk.view.image.rounded.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sinothk.android.utils.UnitUtil;
+
 public class RoundedDemoMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rounded_image_view_main);
+
+
+        UnitUtil.getFileSize(11);
     }
 }
